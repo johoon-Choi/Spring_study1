025f5e65a616530b260e0e8c27881ef06b1308cc
johoon-Choi <johoon4687@gmail.com>
2022-06-23 22:38:03 +0900

    22.06.23

--- src/main/java/JH/hellospring/HelloSpringApplication.java
+++ src/main/java/JH/hellospring/HelloSpringApplication.java
@@ -3,7 +3,7 @@ package JH.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Annotation
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
