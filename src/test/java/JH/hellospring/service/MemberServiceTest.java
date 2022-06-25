825abf2ada84bb92b81e093e0d38f045cf087afe
johoon-Choi <johoon4687@gmail.com>
2022-06-26 00:52:53 +0900

    22.06.26

--- src/test/java/JH/hellospring/service/MemberServiceTest.java
+++ src/test/java/JH/hellospring/service/MemberServiceTest.java
@@ -1,7 +1,9 @@
 package JH.hellospring.service;
 
 import JH.hellospring.domain.Member;
+import JH.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -11,6 +13,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberServiceTest {
 
     MemberService memberService = new MemberService();
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
 
     @Test
     void 회원가입() {   // 한글로도 가능
@@ -26,6 +35,32 @@ class MemberServiceTest {
         // Optional<Member> one = memberService.findOne(saveId);
         Member findMember = memberService.findOne(saveId).get();
         Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e) {
+//            // 여기로 오면 예외가 터져서 정상적으로 온것
+//            // Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다. 123"); // 에러뜸 123 때문에
+//        }
+
+        
+        // then
     }
 
     @Test
