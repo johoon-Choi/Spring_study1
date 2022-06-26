48ecf5f9edacfeb1e9f420f205ddaba3ea605c22
johoon-Choi <johoon4687@gmail.com>
2022-06-27 00:50:06 +0900

    22.06.27

--- src/test/java/JH/hellospring/service/MemberServiceTest.java
+++ src/test/java/JH/hellospring/service/MemberServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -78,10 +79,12 @@ class MemberServiceTest {
     }
 
     @Test
-    void findMembers() {
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
     }
 
     @Test
-    void findOne() {
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
